9cf40893220d09c605d1d0bb9291d5da53f79b35
richmcateer <rmcateer1@gmail.com>
2018-06-27 14:43:27 -0700

    Add CircleCI

--- .circleci/debug.sh
+++ .circleci/debug.sh
@@ -1,6 +1,6 @@
 curl --user 58871610ce9aded55640c426063f9c8ce0f26bdc: \
     --request POST \
-    --form revision=6ba4b0e39a2f32a20699992702deeeb990627909 \
+    --form revision=2d8b73344a32eecfd66e07d5e5a325a540b4a911 \
     --form config=@debug.yml \
     --form notify=false \
-        https://circleci.com/api/v1.1/project/github/marbleprotocol/arbitrage/tree/master
+        https://circleci.com/api/v1.1/project/github/marbleprotocol/arbitrage/tree/circle
