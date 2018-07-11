e81c41a428df9925be0931ebaf74df7ddd267923
richmcateer <rmcateer1@gmail.com>
2018-07-11 16:11:48 -0700

    Add license and titles to Solidity files

--- contracts/Bank.sol
+++ contracts/Bank.sol
@@ -1,10 +1,29 @@
+/*
+
+  Copyright 2018 Contra Labs Inc.
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+
+*/
+
 pragma solidity 0.4.24;
 
 import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
 import "openzeppelin-solidity/contracts/math/SafeMath.sol";
 import "./Transfer.sol";
 
-
+// @title Bank: Accept deposits and allow approved contracts to borrow Ether and ERC20 tokens.
+// @author Rich McAteer <rich@marble.org>, Max Wolff <max@marble.org>
 contract Bank is Ownable, Transfer {
     using SafeMath for uint256;
 
